9c5b75b7943f3c7468fb5c01e3dc538b126eeb99
Zefanya Sendri <zefanyasendri@gmail.com>
2022-05-24 20:26:13 +0700

    Layouting Account and Edit Profile Page

--- Android/BUMI/app/src/main/java/com/dicoding/android/bumi/ui/account/AccountFragment.kt
+++ Android/BUMI/app/src/main/java/com/dicoding/android/bumi/ui/account/AccountFragment.kt
@@ -1,35 +1,56 @@
 package com.dicoding.android.bumi.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dicoding.android.bumi.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.android.bumi.MainActivity
+import com.dicoding.android.bumi.databinding.FragmentAccountBinding
+import com.dicoding.android.bumi.ui.consultation.ConsultationViewModel
+import kotlinx.coroutines.Dispatchers.Main
 
 class AccountFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentAccountBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this)[ConsultationViewModel::class.java]
+
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
+        return binding.root
+
+        setupAction()
+    }
+
+    private fun setupAction() {
+        _binding?.btnEditProfil?.setOnClickListener {
+            Toast.makeText(activity,"Text!",Toast.LENGTH_SHORT).show();
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+
+        _binding?.btnEditProfil?.setOnClickListener {
+            val intent = Intent (this@AccountFragment.context, EditAccountActivity::class.java)
+            startActivity(intent)
+        }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
